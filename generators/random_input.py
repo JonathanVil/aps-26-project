ade0d6a7c7c73aa50429726ed45efee07f5e9408
kassandra <kwad@itu.dk>
2026-04-21 12:37:08 +0200

    wip input generator

--- generators/random_input.py
+++ generators/random_input.py
@@ -1,14 +1,19 @@
 import random
 
-n = int(input())
+A = 5       # auditoriums
+max_p = 50  # max people in an auditorium - also max flow per connection maybe?
+R = random.randint(1,10)      # amount of rooms
+C = 5       # canteens
 
-print(n)
+# generate the auditoriums with [1 - max_p] people in it
+auditoriums = [(f"a{i}", random.randint(1, max_p)) for i in range(A)]
+#print (auditoriums)
 
-indexes = random.sample(range(-1, int(n * 1.02)), n)
-for i, x in enumerate(indexes):
-    if i == len(indexes) - 1:
-        print(x, end="")
-    else:
-        print(i, end=" ")
+# generate ?? rooms with a capacity of [1 - max_p]
+rooms = [(f"r{i}", random.randint(1, max_p)) for i in range(R)]
+#print(rooms)
 
-print("\n", end="")
+# generate C canteens
+canteens = [f"c{i}"for i in range(C)]
+
+# Time to connect 
