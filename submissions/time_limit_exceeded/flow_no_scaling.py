3dbac42db6ba7d219de6132f4547caa7074c8d5a
kassandra <kwad@itu.dk>
2026-05-03 16:31:43 +0200

    add slow submission (no scaling)

---
+++ submissions/time_limit_exceeded/flow_no_scaling.py
@@ -0,0 +1,99 @@
+from collections import defaultdict
+import sys
+
+#sys.setrecursionlimit(10**6)
+
+# all graphs are (default) dictionaries
+# vertex -> (vertex -> capacity), by default capacity is 0
+
+
+def bfs(graph,src,dest,mincap=0): # returns path to dest or reachable set
+    parent = {src:src}
+    layer = [src]
+    while layer:
+        nextlayer = []
+        for u in layer:
+            for v,cap in graph[u].items():
+                if cap > mincap and v not in parent:
+                    parent[v] = u
+                    nextlayer.append(v)
+                    if v == dest:
+                        p =  []
+                        current_vertex = dest
+                        while src != current_vertex:
+                            p.append((parent[current_vertex],current_vertex))
+                            current_vertex = parent[current_vertex]
+                        return (True,p)
+        layer = nextlayer
+    return (False,set(parent))
+
+def dfs(graph,u,dest,mincap,seen): # returns path to dest
+    if u in seen:
+        return (False,seen)
+    seen.add(u)
+    for v,cap in graph[u].items():
+        if cap > mincap: # only consider edges with capacity > mincap
+            if v == dest:
+                return (True,[(u,v)])
+            #print(f'explore {u} {v}, {cap}')
+            suc, p = dfs(graph,v,dest,mincap,seen)
+            if suc:
+                p.append((u,v))
+                return (True,p)
+    return (False,seen)
+
+def flow(orggraph, src,dest):
+    graph = defaultdict(lambda: defaultdict(int))
+    maxcapacity = 0
+    for u,d in orggraph.items():
+        for v,c in d.items():
+            graph[u][v] = c
+            maxcapacity = max(maxcapacity,c)
+
+    current_flow = 0
+    mincap = 0
+    while True:
+        #ispath, p_or_seen = bfs(graph,src,dest,mincap)
+        ispath, p_or_seen = dfs(graph,src,dest,mincap, set())
+        if not ispath:
+            if mincap > 0:
+                mincap = mincap // 2
+                continue
+            else:
+                return (current_flow,
+                        { a:{b:c-graph[a][b] for b,c in d.items() if graph[a][b]<c} 
+                            for a,d in orggraph.items() },
+                        p_or_seen)
+        p = p_or_seen
+        #print("path:", *reversed(p))
+        saturation = min( graph[u][v] for u,v in p )
+        #print(current_flow,saturation)#,[f"{u[0]}-{u[1]}:{orggraph[u[0]][u[1]]}:{graph[u][v]}" for u,v in p if u[2]==0])
+        current_flow += saturation
+        for u,v in p:
+            graph[u][v] -= saturation
+            graph[v][u] += saturation
+
+
+graph = defaultdict(lambda: defaultdict(int))
+A = int(input())
+
+for _ in range(A):
+    name, p = input().split()
+    capacity = int(p)
+    graph["supersource"][name] = capacity
+
+C = int(input())
+for _ in range(C):
+    name = input()
+    graph[name]["supersink"] = sys.maxsize
+
+D = int(input())
+for _ in range(D):
+    src, dst, p= input().split()
+    capacity = int(p)
+    graph[src][dst] = capacity
+
+
+flowvalue, residual_graph, cut = flow(graph, "supersource", "supersink")
+print(flowvalue)
+
